417da6cb8e61900b317c0caba9b8e5ff205db69a
blurHY <blurhy@outlook.com>
2019-01-01 18:11:34 +0800

    Update HorizonSpider.py

--- HorizonSpider.py
+++ HorizonSpider.py
@@ -20,7 +20,7 @@ try:
         "1HeLLo4uzjaLetFx6NH3PMwFP3qbRbTf3D")
 except:
     logger.warning("ZeroHello has not been downloaded yet")
-    requests.get("http://127.0.0.1:43110", headers={"ACCEPT": "text/html"})
+    requests.get("http://"+ZeroNetAddr, headers={"ACCEPT": "text/html"})
     while True:
         try:
             ZeroHelloKey = ZiteUtils.getWrapperkey(
