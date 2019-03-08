5a3b24e5022924df83644d22e16527068abd684a
blurHY <blurhy@outlook.com>
2019-03-08 23:04:58 +0800

    Crawlers become plugins and use promise pool

--- App.js
+++ App.js
@@ -9,100 +9,11 @@ const SiteDB = require("./SiteDataBase")
 const SiteMeta = require("./SiteMeta")
 const PromisePool = require("es6-promise-pool")
 
+let modules = require("require-dir-all")("Crawlers")
 
 let admin = new Admin()
 
 let exiting = false
-
-async function updateFeeds(dbSchema, siteDB, siteObj) {
-    if (!siteDB || !dbSchema || !dbSchema.feeds) // Always check params in final function
-        return
-
-    log("info", "spider", `Feeds available: ${siteObj.basicInfo.address}`)
-
-    let query, func = s => s
-
-    let maxDate = Math.max.apply(Math, siteObj.feedsQueried.map(o => o.result.date_added))
-    if (!(siteObj.runtimeInfo.lastCrawl.feeds.full < new Date() - process.env.feedFull_Period) && maxDate > 0) {
-        if (siteObj.runtimeInfo.lastCrawl.feeds.check < new Date() - process.env.feedCheck_Peroid) { // New feeds only
-            siteObj.runtimeInfo.lastCrawl.feeds.check = new Date()
-            func = s => `SELECT * FROM (${s}) where date_added > ${maxDate}` // Not needed to add outer where clause into inner, because of the sqlite optimization
-        } else
-            log("info", "spider", `Stored feeds are up to date ${siteObj.basicInfo.address}`)
-    } else {
-        siteObj.feedsQueried.splice(0) // Clear old data and re-query all feeds
-        siteObj.runtimeInfo.lastCrawl.feeds.full = new Date()
-    }
-
-    for (let name in dbSchema.feeds) {
-        if (!name)
-            continue
-        query = func(dbSchema.feeds[name])
-
-        await pagingFeedQuery(query, siteDB, siteObj, name)
-    }
-    log("info", "spider", `Updated feeds for ${siteObj.basicInfo.address}`)
-}
-
-async function pagingFeedQuery(query, siteDB, siteObj, name, count = 3000, start = 0) {
-    let ori_query = query
-    query = `select * from (${query}) limit ${count} offset ${start}` // Sqlite has powerful optimization, so we have do it like this.
-    siteDB.all(query, async (err, rows) => {
-            if (err || !(rows instanceof Array)) {
-                log("error", "spider", "An error occurred during a feed query", err)
-            } else if (rows.length > 0) {
-
-                let renamedRows = []
-                for (let row of rows) {
-                    renamedRows.push({
-                        itemType: row.type,
-                        date_added: row.date_added,
-                        title: row.title,
-                        body: row.body,
-                        url: row.url
-                    })
-                }
-
-                siteObj = await DataBase.getSite(siteObj.basicInfo.address) // Refresh model
-
-                if (!siteObj)
-                    return
-
-                let feedObj = false
-                let index = 0
-
-                for (let ele of siteObj.feedsQueried) {
-                    if (ele.name === name) {
-                        feedObj = ele
-                        break
-                    }
-                    index++
-                }
-
-                if (!feedObj)
-                    await DataBase.siteModel.update(
-                        {"basicInfo.address": siteObj.basicInfo.address},
-                        {$push: {feedsQueried: {name, result: renamedRows}}}
-                    )
-                else {
-                    await DataBase.siteModel.update(
-                        {"basicInfo.address": siteObj.basicInfo.address},
-                        {$push: {[`feedsQueried.${index}.result`]: {$each: renamedRows}}}
-                    )
-                }
-
-                log("info", "spider", `Imported ${rows.length} feeds from ${siteObj.basicInfo.address}`)
-                await pagingFeedQuery(ori_query, siteDB, siteObj, name, count, start + count) // Query and store next page
-            }
-        }
-    )
-}
-
-async function updateOptionalFiles(siteDB, siteObj) {
-    if (!siteDB)
-        return
-    // TODO: For each content.json and get optional files.Get details from database
-}
 
 async function crawlASite(site) {
     try {
@@ -116,7 +27,15 @@ async function crawlASite(site) {
             siteObj = DataBase.genNewSite(site)
         }
 
-        await Promise.all([updateFeeds(dbSchema, siteDB, siteObj), updateOptionalFiles(siteDB, siteObj)])
+        function* promiseGenerator() {
+            for (let crawler in modules) {
+                if (modules[crawler] && modules[crawler].crawl)
+                    yield (async () => await modules[crawler].crawl(dbSchema, siteDB, siteObj))()
+            }
+        }
+
+        let pool = new PromisePool(promiseGenerator, parseInt(process.env.Concurrency) || 3)
+        await pool.start()
 
         await siteObj.save()
         log("info", "spider", `Saved site ${site.address}`)
---
+++ Crawlers/OptionalFiles.js
@@ -0,0 +1,7 @@
+async function updateOptionalFiles(dbSchema, siteDB, siteObj) {
+    if (!siteDB)
+        return
+    // TODO: For each content.json and get optional files.Get details from database
+}
+
+module.exports = {crawl: updateOptionalFiles}
