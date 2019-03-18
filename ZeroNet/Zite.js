8f71cb4012dae097b125099f7ed2c26b680e4ad7
blurHY <blurhy@outlook.com>
2019-03-18 17:01:04 +0800

    Update Zite.js

--- ZeroNet/Zite.js
+++ ZeroNet/Zite.js
@@ -1,12 +1,13 @@
-const ZeroWs = require('./ZeroWs');
-const Settings = require('./SettingsLoader');
-const getWrapperKey = require("./SitesJson").getWrapperKey;
-const log = require('../Logger')
+const ZeroWs = require("./ZeroWs")
+const Settings = require("./SettingsLoader")
+const getWrapperKey = require("./SitesJson").getWrapperKey
+const log = require("../Logger")
 
 
 module.exports = class Zite extends ZeroWs {
     constructor(site_addr) {
-        log('info', "zeronet", "Getting wrapper key for " + site_addr)
+        log("info", "zeronet", "Getting wrapper key for " + site_addr)
+        log("info", "zeronet", `ZeroNetHost: ${Settings.ZeroNetHost}`)
         super(getWrapperKey(site_addr), Settings.ZeroNetHost)
     }
 }
