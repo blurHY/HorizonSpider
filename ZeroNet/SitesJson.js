b4294d63279e37f062b915055ce2fdb3d3b0a2a4
blurHY <blurhy@outlook.com>
2019-03-18 19:57:28 +0800

    Update SitesJson.js

--- ZeroNet/SitesJson.js
+++ ZeroNet/SitesJson.js
@@ -1,21 +1,22 @@
 const readFileSync = require("fs").readFileSync
 const join = require("path").join
-const signale = require('signale');
+const signale = require("signale")
 
-let obj = null
 
 module.exports = {
     reloadJson() {
         try {
-            obj = JSON.parse(readFileSync(join(process.env.ZeroNetPath, "data", "sites.json"), "utf-8"))
+            global.sitesJson = JSON.parse(readFileSync(join(process.env.ZeroNetPath, "data", "sites.json"), "utf-8"))
         } catch (e) {
             signale.error("Cannot load sites json: " + e)
         }
     },
     getWrapperKey(site_addr) {
-        if (!obj)
+        if (!global.sitesJson)
             module.exports.reloadJson()
-        return obj && obj[site_addr] ? obj[site_addr].wrapper_key : null
-    },
-    obj
+        if (global.sitesJson && global.sitesJson[site_addr])
+            return global.sitesJson[site_addr].wrapper_key
+        else
+            throw `No wrapper_key for ${site_addr}`
+    }
 }
